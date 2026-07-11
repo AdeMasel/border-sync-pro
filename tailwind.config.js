c631fa89cbd52ecbc8e6b3e9441a81b8b6a735fc
AdeMasel <ademase@gmail.com>
2026-07-11 12:57:36 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./src/renderer/**/*.{js,jsx,ts,tsx,html}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace']
+      }
+    }
+  },
+  plugins: []
+}
